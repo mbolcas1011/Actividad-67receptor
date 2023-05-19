6294197da909fc4474a49a67e16b71fff5567e11
mbolcas1011 <132547406+mbolcas1011@users.noreply.github.com>
2023-05-19 12:33:10 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    let recivedNumber = 0
+    led.setBrightness(led.brightness() + recivedNumber)
+})
+radio.onReceivedString(function (receivedString) {
+    let recivedString = ""
+    if (recivedString == "POWER") {
+        encendido = !(encendido)
+        led.setBrightness(125)
+        led.enable(encendido)
+    }
+})
+let encendido = false
+radio.setGroup(1)
+encendido = false
+led.enable(encendido)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
